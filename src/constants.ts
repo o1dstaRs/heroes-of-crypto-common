0e8c8c6966dac23b324d6186101999443c475727
Stepan Zolotukhin <stepan.zolotukhin@yahoo.com>
2024-10-06 13:35:23 -0700

    Move UnitsHolder and FightStateManager from the client code

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,14 @@
+/*
+ * -----------------------------------------------------------------------------
+ * This file is part of the common code of the Heroes of Crypto.
+ *
+ * Heroes of Crypto and Heroes of Crypto AI are registered trademarks.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * -----------------------------------------------------------------------------
+ */
+
 // damage
 export const PENALTY_ON_RANGE_SHOT_THROUGH_TEAMMATES = false;
 export const MIN_UNIT_STACK_POWER = 1;
---
+++ src/fights/fight_state_manager.ts
@@ -0,0 +1,40 @@
+/*
+ * -----------------------------------------------------------------------------
+ * This file is part of the common code of the Heroes of Crypto.
+ *
+ * Heroes of Crypto and Heroes of Crypto AI are registered trademarks.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * -----------------------------------------------------------------------------
+ */
+
+import { FightProperties } from "./fight_properties";
+
+export class FightStateManager {
+    private static instance: FightStateManager;
+
+    // TODO: control units here too
+
+    private fightProperties: FightProperties;
+
+    private constructor() {
+        this.fightProperties = new FightProperties();
+    }
+
+    public reset(): void {
+        this.fightProperties = new FightProperties();
+    }
+
+    public static getInstance(): FightStateManager {
+        if (!FightStateManager.instance) {
+            FightStateManager.instance = new FightStateManager();
+        }
+
+        return FightStateManager.instance;
+    }
+
+    public getFightProperties(): FightProperties {
+        return this.fightProperties;
+    }
+}
